839a1bb20d08a45113c906d33b14f07dadde25ca
Vasya-556 <91149467+Vasya-556@users.noreply.github.com>
2022-04-20 22:18:15 +0300

    Add files via upload

---
+++ labs-8.1.3.cpp
@@ -0,0 +1,39 @@
+﻿// Labs-8.1.3.cpp
+// Кобрин Василь
+// Лабораторна робота No 8.1.3
+// Пошук та заміна символів у літерному рядку
+// Варіант 3
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int Count(char* str, int i, int l, int k)
+{
+	if (strlen(str) < 3)
+		return 0;
+
+
+	int j = 0;
+
+	if (str[i + 1] != 0)
+		if (str[i] == ',')
+			if (k == 3)
+				return l + Count(str, i + 1, l + 1, k+1);
+		else
+			return Count(str, i + 1, l + 1, k + 1);
+		else
+			return Count(str, i + 1, l+1, k);
+	else
+		return 0;
+}
+
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "third coma: " << Count(str, 1, 1, 1) << endl;
+	return 0;
+}
